5a6c33967cef5569b767f965af32e888dbce3e32
amr <elghamrawyamr43@gmail.com>
2023-04-05 23:38:36 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::resource('posts', \App\Http\Controllers\PostController::class);
+    Route::inertia('pages/about', 'About')->name('pages.about');
+});
+
+Route::inertia('login', 'Auth/Login')->name('login');
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'store'])
+    ->name('login.post');
+Route::post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'destroy'])
+->name('logout');
